7fd27e7a1e653f0a0b70886f952ed9f018eab47a
Anthony <dev.autumnsky@gmail.com>
2021-08-10 15:48:49 +0900

    Test Toast Editor

--- src/components/ToastEditor.vue
+++ src/components/ToastEditor.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="toast-editor">
+  <div class="flex justify-center">
+    <div class="w-1/2">
+      <Editor :previewStyle="'tab'" />
+    </div>
   </div>
 </template>
 
 <script>
-import Editor from '@toast-ui/editor'
-import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/toastui-editor.css'
+import { Editor } from '@toast-ui/vue-editor'
+
+// ISSUE : Insert Image base64
 
 export default {
-  name: 'ToastEditor',
-  data() {
-    return {
-      editor: null
-    }
-  },
-  mounted() {
-    this.editor = new Editor({
-      el: document.querySelector('#toast-editor'),
-      height: '500px',
-      initialEditType: 'markdown',
-      previewStyle: 'vertical'
-    })
-    this.editor.getMarkdown()
+  components: {
+    Editor
   }
 }
 </script>
